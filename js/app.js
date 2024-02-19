2240d8a63046477ac9475613b451080034ae6b56
hasanali207 <hasanali142228@gmail.com>
2024-02-19 10:52:28 +0600

    added functionality

--- js/app.js
+++ js/app.js
@@ -1,16 +1,29 @@
-const AllseatName = document.getElementsByClassName('seat-name');
+const allseatName = document.getElementsByClassName('seat-name');
 let selectSeatNumber = 0;
-const ticketPrice = document.getElementById('ticket-price').innerText
-for(seatName of AllseatName){
+const ticketPrice = getTextElementValueById('ticket-price')
+let SeatNumber = 4
+for(seatName of allseatName){
         seatName.addEventListener('click', function(event){
         
         event.target.style.backgroundColor = '#1DD100';
         event.target.style.color = '#fff';
         event.target.style.borderRadius = '6px';
-        event.target.setAttribute('disabled', 'true')
+        event.target.style.cursor = 'not-allowed';
+        event.target.style.pointerEvents = 'none';
         
+    
         selectSeatNumber = selectSeatNumber + 1;
         setInnerText('select-seat-number', selectSeatNumber)
+
+    // SeatNumberCoundown up to down
+        const availableSeatNumber = getTextElementValueById('available-seat');
+        const updatedSeat = availableSeatNumber - 1;
+        setInnerText('available-seat', updatedSeat);
+        if(updatedSeat === 0){
+            alert('No Seat Is Avaiable');
+           
+              }
+        
 // Append Content 
     const getSeatName = event.target.innerText;           
     const appendContainer = document.getElementById('append-selected-seat') ;   
@@ -28,16 +41,69 @@ for(seatName of AllseatName){
     newElement.appendChild(p2)
     newElement.appendChild(p3)
     appendContainer.appendChild(newElement)
-    console.log(newElement);
- const tatalCost = document.getElementById('tatal-cost').innerText
-    
 
 
+ let totalCost =  getTextElementValueById('total-cost');
+     let  sumOfTotalCost = totalCost + ticketPrice
+      
+      setInnerText('total-cost',sumOfTotalCost)
+
+      let grandTotal = getTextElementValueById('grand-total');
+       let grandTotalCost = grandTotal + ticketPrice;
+      setInnerText('grand-total', grandTotalCost)
+
+
+      const calculateButton = document.getElementById('calculate-btn');
+        const discountCodeInput = document.getElementById('discount-code')
+        calculateButton.addEventListener('click', function(event){
+            
+            const couponCodeWrapper = document.getElementById('coupon-code-area');
+            const discountCode = discountCodeInput.value
+           if(SeatNumber <= selectSeatNumber){
+            if(discountCode === "NEW15"){
+                grandTotalCost = grandTotalCost - (grandTotalCost * 0.15)
+                setInnerText('grand-total', grandTotalCost)
+                couponCodeWrapper.style.display = 'none'
+            }
+            else if(discountCode === "Couple20"){
+                grandTotalCost = grandTotalCost - (grandTotalCost * 0.20)
+                setInnerText('grand-total', grandTotalCost)
+                couponCodeWrapper.style.display = 'none'
+            }
+            
+           }
+
+            
+        })
+
+
+        const phone = document.getElementById('phone');
+        const submitButton = document.getElementById('submit-btn')
+        submitButton.querySelector('click', function(){
+            event.par
+        })
+
+      
+      
+
+  
     })
 }
 
+
+
+
 function setInnerText(id,value){
     document.getElementById(id).innerText = value;
+    return;
+}
+
+
+function getTextElementValueById(elementId){
+    const element = document.getElementById(elementId);
+    const elementValueText = element.innerText;
+    const value = parseInt(elementValueText);
+    return value;
 }
 
 
@@ -54,12 +120,11 @@ function setInnerText(id,value){
 // function getInputValueById(inputFieldId){
 //     const inputField = document.getElementById(inputFieldId);
 //     const inputValueText = inputField.value;
-//     const inputValue = parseFloat(inputValueText);
 //     // console.log(inputValue);
-//     return inputValue;
+//     return inputValueText;
 // }
 
-// function setInnerTextById(elementId, area){
-//     const element = document.getElementById(elementId);
-//     element.innerText = area;
-// }
+// // function setInnerTextById(elementId, area){
+// //     const element = document.getElementById(elementId);
+// //     element.innerText = area;
+// // }
